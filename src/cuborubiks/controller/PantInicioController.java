5cabdaef028088f1aefdb3a0de3054450e5e0ede
kevin-fallas-b <kevin.fallas.b@gmail.com>
2018-08-10 19:27:38 -0600

    creacion pant inicio

---
+++ src/cuborubiks/controller/PantInicioController.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuborubiks.controller;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author kevin
+ */
+public class PantInicioController implements Initializable {
+
+    @FXML
+    private Button btnAyuda;
+    @FXML
+    private JFXButton btnAcercaDe;
+    @FXML
+    private JFXButton btnIniciar;
+    @FXML
+    private JFXButton btnCargarPartida;
+    @FXML
+    private JFXButton btnModificarPartida;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void presionarBtnAyuda(ActionEvent event) {
+    }
+
+    @FXML
+    private void presionarBtnAcercaDe(ActionEvent event) {
+    }
+
+    @FXML
+    private void presionarBtnIniciar(ActionEvent event) {
+    }
+
+    @FXML
+    private void presionarBtnCargarPartida(ActionEvent event) {
+    }
+
+    @FXML
+    private void presionarBtnModificarPartida(ActionEvent event) {
+    }
+    
+}
