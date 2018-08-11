8f1a81373b9c5b577733db5041802a4da362095f
kevin-fallas-b <kevin.fallas.b@gmail.com>
2018-08-10 21:14:14 -0600

    final primer dia

--- src/cuborubiks/controller/PantInicioController.java
+++ src/cuborubiks/controller/PantInicioController.java
@@ -13,7 +13,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Region;
 
 /**
  * FXML Controller class
@@ -32,6 +34,8 @@ public class PantInicioController extends Controller implements Initializable {
     private JFXButton btnCargarPartida;
     @FXML
     private JFXButton btnModificarPartida;
+    @FXML
+    private JFXButton btnTablaPuntaje;
 
     /**
      * Initializes the controller class.
@@ -48,6 +52,13 @@ public class PantInicioController extends Controller implements Initializable {
 
     @FXML
     private void presionarBtnAyuda(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        alert.setResizable(true);
+        alert.setTitle("Ayuda");
+        alert.setHeaderText("Este es el dialogo de ayuda\nAbajo se detalla la funcion de cada boton.");
+        alert.setContentText("-Iniciar: Inicia una partida nueva con los colores predeterminados.\n\n-Cargar Partida: Permite buscar una partida guardada anteriormente y continuarla.\n\n-Modificar Partida: Permite que el usuario modifique los colores del cubo antes de iniciar la partida.\n\n-Tabla de puntaje: Permite al usuario observar cuales han sido los mejores puntajes obtenidos.\n\n-Acerca de: Muestra informacion acerca de quien desarrollo este software.");
+        alert.showAndWait();
     }
 
     @FXML
@@ -67,6 +78,11 @@ public class PantInicioController extends Controller implements Initializable {
     @FXML
     private void presionarBtnModificarPartida(ActionEvent event) throws IOException {
         FlowController.getInstance().goViewSameWindow("PantModificarCubo", btnModificarPartida);
+    }
+
+    @FXML
+    private void presionarBtnTablaPuntaje(ActionEvent event) throws IOException {
+        FlowController.getInstance().goViewSameWindow("PantTablaPuntaje", btnTablaPuntaje);
     }
 
 }
